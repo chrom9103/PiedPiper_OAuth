2a102fb8c0b225658b2d9e54df6c3fd43bd058c2
chrom9103 <kdkd21347.tm@gmail.com>
2025-05-26 17:13:22 +0900

    roleIDを修正

--- main.py
+++ main.py
@@ -34,51 +34,53 @@ async def on_member_join(member):
         if str(member.id) in allowed_members:
             print(str(member.id))
             #memberを付与
-            role_name = "member"
-            role = discord.utils.get(guild.roles, name=role_name)
+            load_dotenv('./datas/main_roleID.env')
+            target_role_id = os.getenv("member")
+            role = discord.utils.get(guild.roles, id=int(target_role_id))
             if role is not None:
                 try:
                     await member.add_roles(role)
-                    print(f"Role {role_name} has been granted to {member.name}.")
+                    print(f"Role {role.name} has been granted to {member.name}.")
                 except discord.Forbidden:
-                    print(f"Permission error: {role_name} role cannot be granted to {member.name}.")
+                    print(f"Permission error: {role.name} role cannot be granted to {member.name}.")
                 except discord.HTTPException as e:
                     print(f"HTTP error: {e}")
             else:
-                print(f"Role '{role_name}' was not found in guild {member.guild.name}.")
+                print(f"Role '{role.name}' was not found in guild {member.guild.name}.")
         else:
             #pre-memberを付与
-            role_name = "pre-member"
-            role = discord.utils.get(guild.roles, name=role_name)
+            load_dotenv('./datas/main_roleID.env')
+            target_role_id = os.getenv("pre-member")
+            role = discord.utils.get(guild.roles, id=int(target_role_id))
             if role is not None:
                 try:
                     await member.add_roles(role)
-                    print(f"Role {role_name} has been granted to {member.name}")
+                    print(f"Role {role.name} has been granted to {member.name}")
                 except discord.Forbidden:
                     print("Permission error: role cannot be granted.")
                 except discord.HTTPException as e:
                     print(f"HTTP error: {e}")
             else:
-                print(f"Role '{role_name}' was not been found.")
+                print(f"Role '{role.name}' was not been found.")
     except FileNotFoundError:
         print(f"File '{member_list_file}' not found.") 
 
 async def save_member_list():
-    target_guild_id = 1304058364560543815
-    target_role_name = "member"
-    member_list_file = "member_list.txt"
+    load_dotenv('./datas/main_roleID.env')
+    target_role_id = os.getenv("member")
 
+    target_guild_id = 1304058364560543815
     guild = bot.get_guild(target_guild_id)
     if guild:
-        role = discord.utils.get(guild.roles, name=target_role_name)
+        role = discord.utils.get(guild.roles, id=int(target_role_id))
         if role:
             members_with_role = [member.id for member in guild.members if role in member.roles]
             with open("datas/member_list.txt", "w") as file:
                 for member in members_with_role:
                     file.write(f"{member}\n")
-            print(f"Member list saved to '{member_list_file}'.")
+            print(f"Member list saved.")
         else:
-            print(f"Role '{target_role_name}' not found in guild.")
+            print(f"Role '{role.name}' not found in guild.")
     else:
         print(f"Guild with ID {target_guild_id} not found.")
 
